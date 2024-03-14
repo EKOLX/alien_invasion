5e644056db96b62f8addb5ed3480be794844dc83
EL Khan <elxanmursal@gmail.com>
2024-03-14 22:03:15 +0400

    Added game stats and losing logic.

--- settings.py
+++ settings.py
@@ -7,16 +7,19 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (210, 220, 230)
+        self.screen_pause_time = 1.0
 
         # Ship settings
         self.ship_speed = 2.3
+        self.ship_limit = 3
 
         # Bullet settings
-        self.bullet_speed = 2.0
+        self.bullet_speed = 2.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (65, 66, 67)
         self.bullets_allowed = 3
+        self.bullet_destroyed_on_hit = True
 
         # Alien settings
         self.alien_speed = 1.0
